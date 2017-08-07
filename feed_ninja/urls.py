3634d237c8c4317ad674c557dd87d314ddf76918
Manoj Mohan <m.manoj@linuxmail.org>
2017-08-07 18:45:46 +0530

    added get_title method and update_sources method

--- feed_ninja/urls.py
+++ feed_ninja/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'test_page', views.test_view)
+    url(r'get_title', views.get_title)
     ]
--- feed_ninja/views.py
+++ feed_ninja/views.py
@@ -5,18 +5,35 @@ import json
 import logging
 from django.views.decorators.csrf import csrf_protect, csrf_exempt
 
-
+feed_urls = {
+    'FSFTN': 'https://fsftn.org/blog/rss/',
+    'norvig': 'http://norvig.com/rss-feed.xml'
+}
+aggregate_feed_objects= []
 logger = logging.getLogger(__name__)
 
 
 @csrf_exempt
-def test_view(request):
+def get_title(request):
     json_input = json.loads(request.body)
-    logger.error(json_input['name'], exc_info=1)
-    result = feedparser.parse('https://fsftn.org/blog/rss/')
-    result = result['entries'][0]
+    recent = json_input['recent']
+    update_sources()
+    result = []
+
+    for feed in aggregate_feed_objects:
+        for entry in feed['entries']:
+            result.append(entry['title'])
+
     result_json = json.dumps(result, ensure_ascii=False)
     return HttpResponse(result_json,
                         content_type='application/json; charset=utf-8')
-#    return HttpResponse(result_json,
-#                        content_type='application/json; charset=utf-8')
+
+
+def update_sources():
+
+    for feed_url in feed_urls.values():
+        aggregate_feed_objects.append(feedparser.parse(feed_url))
+
+
+
+
