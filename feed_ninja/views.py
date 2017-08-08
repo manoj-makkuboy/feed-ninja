d63d533c79821357d699a21e7011c88129171daf
Manoj Mohan <m.manoj@linuxmail.org>
2017-08-08 19:47:46 +0530

    sequential implementation using asyncio

--- feed_ninja/views.py
+++ feed_ninja/views.py
@@ -4,12 +4,15 @@ import feedparser
 import json
 import logging
 from django.views.decorators.csrf import csrf_protect, csrf_exempt
+import asyncio
+import time
+
 
 feed_urls = {
     'FSFTN': 'https://fsftn.org/blog/rss/',
     'norvig': 'http://norvig.com/rss-feed.xml'
 }
-aggregate_feed_objects= []
+aggregate_feed_objects = []
 logger = logging.getLogger(__name__)
 
 
@@ -18,6 +21,7 @@ def get_title(request):
     json_input = json.loads(request.body)
     recent = json_input['recent']
     update_sources()
+#    time.sleep(2)
     result = []
 
     for feed in aggregate_feed_objects:
@@ -28,12 +32,22 @@ def get_title(request):
     return HttpResponse(result_json,
                         content_type='application/json; charset=utf-8')
 
+async def make_task(url):
+    logger.error("start",url)
+    aggregate_feed_objects.append(await my_coroutine(url))
+    logger.error("stop",url)
 
+async def my_coroutine(url):
+#    await asyncio.sleep(1) #asyncio.coroutine(feedparser.parse(url))
+    return  feedparser.parse(url)
 def update_sources():
 
-    for feed_url in feed_urls.values():
-        aggregate_feed_objects.append(feedparser.parse(feed_url))
-
-
-
-
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    global aggregate_feed_objects
+    aggregate_feed_objects = []
+    tasks = [asyncio.ensure_future(make_task('https://fsftn.org/blog/rss/')),
+             asyncio.ensure_future(make_task('http://norvig.com/rss-feed.xml'))]
+    wait_tasks = asyncio.wait(tasks)
+    loop.run_until_complete(wait_tasks)
+#    loop.close()
