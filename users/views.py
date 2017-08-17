905947969a9e16e1f68156fc52c97eb68bc33775
Manoj Mohan <m.manoj@linuxmail.org>
2017-08-17 14:11:07 +0530

    added post method to add new user

--- users/views.py
+++ users/views.py
@@ -2,6 +2,7 @@ from .serializers import UserSerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.contrib.auth.models import User
+from rest_framework import status
 # Create your views here.
 
 
@@ -15,4 +16,10 @@ class UserList(APIView):
         return Response(serializer_class.data)
 
     def post(self, request):
-        pass
+        serialized_data = UserSerializer(data=request.data)
+        if serialized_data.is_valid():
+            serialized_data.save()
+            return Response(serialized_data.data,
+                            status=status.HTTP_201_CREATED)
+        return Response(serialized_data.errors,
+                        status=status.HTTP_400_BAD_REQUEST)
