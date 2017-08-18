931911ca5f386428babb737325e8ea960d2330e7
Manoj Mohan <m.manoj@linuxmail.org>
2017-08-18 11:46:48 +0530

    added docstring to views and made urls regex more accurate

--- feed_ninja/urls.py
+++ feed_ninja/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'articles', views.Articles.as_view()),
-    url(r'update_load', views.update_load_feeds),
+    url(r'^articles$', views.Articles.as_view()),
+    url(r'^update_load$', views.update_load_feeds),
     ]
--- feed_ninja/views.py
+++ feed_ninja/views.py
@@ -23,12 +23,14 @@ file_save_path = '/home/manoj.mohan/Downloads/feeds/'
 
 
 def file_name_generator(url):
+    ''' Returns the key of global feed_urls for the given value of dictionary'''
     for key, value in feed_urls.items():
         if value == url:
             return key
 
 
 async def download_feeds(session, url):
+    ''' Coroutine which downloads feeds from url given in the argument'''
     with async_timeout.timeout(10):
         async with session.get(url) as response:
             file_name = file_name_generator(url)
@@ -43,12 +45,15 @@ async def download_feeds(session, url):
 
 
 async def main_async_call_to_coroutines(loop):
+    ''' Coroutine which sets the task inside the event loop '''
     async with aiohttp.ClientSession(loop=loop) as session:
         tasks = [download_feeds(session, url) for url in feed_urls.values()]
         await asyncio.gather(*tasks)
 
 
 def load_feed_from_files():
+    ''' reads the feed files from the local filesystem and loads it to the
+    global variable aggregate_feed_objects '''
     for url_name in feed_urls.keys():
         full_file_path = file_save_path + url_name
         aggregate_feed_objects.append(feedparser.parse(full_file_path))
@@ -96,5 +101,7 @@ class Articles(View):
 
 @csrf_exempt
 def update_load_feeds(request):
+    ''' Function Based Views which is invoked when /feed_ninja/update_load is
+    called '''
     update_sources()
     return HttpResponse('Done')
